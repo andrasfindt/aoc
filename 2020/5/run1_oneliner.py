9a8d2088f576383096aaee280bb9cef8409f5793
Andras Findt <andras.findt@standardbank.co.za>
2020-12-05 10:49:00 +0200

    more cleaning

--- 2020/5/run1_oneliner.py
+++ 2020/5/run1_oneliner.py
@@ -1,7 +1,1 @@
-print(max([int(boarding_pass, 2) for boarding_pass in
-           open('input.txt', 'r').read()
-          .replace("F", "0")
-          .replace("B", "1")
-          .replace("R", "1")
-          .replace("L", "0")
-          .split("\n")]))
+print(max([int(boarding_pass, 2) for boarding_pass in open('input.txt', 'r').read().replace("F", "0").replace("B", "1").replace("R", "1").replace("L", "0").split("\n")]))

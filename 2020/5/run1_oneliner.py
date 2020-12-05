428e4990db421317134e3f98b347670739717cf1
Andras Findt <andras.findt@standardbank.co.za>
2020-12-05 10:36:12 +0200

    making slightly better use of python's list magic

---
+++ 2020/5/run1_oneliner.py
@@ -0,0 +1,7 @@
+print(max([int(boarding_pass, 2) for boarding_pass in
+           open('input.txt', 'r').read()
+          .replace("F", "0")
+          .replace("B", "1")
+          .replace("R", "1")
+          .replace("L", "0")
+          .split("\n")]))
--- 2020/5/run2.py
+++ 2020/5/run2.py
@@ -1,13 +1,10 @@
-boarding_passes = open('input.txt', 'r').read() \
-    .replace("F", "0") \
-    .replace("B", "1") \
-    .replace("R", "1") \
-    .replace("L", "0") \
-    .split("\n")
-
-pass_ids = []
-for boarding_pass in boarding_passes:
-    pass_ids.append(int(boarding_pass, 2))
+pass_ids = [int(boarding_pass, 2) for boarding_pass in
+            open('input.txt', 'r').read()
+                .replace("F", "0")
+                .replace("B", "1")
+                .replace("R", "1")
+                .replace("L", "0")
+                .split("\n")]
 
 all_ids = set(range(min(pass_ids), max(pass_ids) + 1))
 missing_ids = all_ids ^ set(pass_ids)

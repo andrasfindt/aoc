e2506cb059d45fbfd3e746d56bfc81aebb6eb2d5
Andras Findt <andras.findt@standardbank.co.za>
2020-12-06 11:06:39 +0200

    removed unnecessary call to replace

--- 2020/6/run1.py
+++ 2020/6/run1.py
@@ -1,1 +1,1 @@
-print(sum([len(set(customs_form)) for customs_form in open('input.txt', 'r').read().replace("\n\n", "|").replace("\n", "").split("|") if customs_form]))
+print(sum([len(set(customs_form)) for customs_form in open('input.txt', 'r').read().split("\n\n") if customs_form]))

0402f2438a1da7fc20b77d9755737956b2f231ed
Andras Findt <andras.findt@standardbank.co.za>
2020-12-06 10:56:38 +0200

    removed unnecessary call to replace

--- 2020/6/run2.py
+++ 2020/6/run2.py
@@ -1,1 +1,1 @@
-print(sum([len(set.intersection(*[set(form) for form in group.split(" ")])) for group in open('input.txt', 'r').read().replace("\n\n", "|").replace("\n", " ").split("|")]))
+print(sum([len(set.intersection(*[set(form) for form in group.split("\n")])) for group in open('input.txt', 'r').read().replace("\n\n", "|").split("|") if group]))

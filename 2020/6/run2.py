0b1aef55b0304ebd61f71aaa2833d80d1dbe021a
Andras Findt <andras.findt@standardbank.co.za>
2020-12-06 10:44:34 +0200

    run2.py as oneliner

--- 2020/6/run2.py
+++ 2020/6/run2.py
@@ -1,10 +1,1 @@
-groups = open('input.txt', 'r').read().replace("\n\n", "|").replace("\n", " ").split("|")
-valid_forms_count = []
-for group in groups:
-    if group:
-        forms = [set(form) for form in group.split(" ")]
-        intersection = forms[0]
-        for form in forms:
-            intersection = intersection.intersection(form) # this is probably a bit expensive...
-        valid_forms_count.append(len(intersection))
-print(sum(valid_forms_count))
+print(sum([len(set.intersection(*[set(form) for form in group.split(" ")])) for group in open('input.txt', 'r').read().replace("\n\n", "|").replace("\n", " ").split("|")]))

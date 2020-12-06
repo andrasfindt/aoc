d9b626b5de82281ff3f7684372ffea0411c264d4
Andras Findt <andras.findt@standardbank.co.za>
2020-12-06 12:20:47 +0200

    part 1 using reduce to multiply. seems kinda of silly compared to the previous version... oh well. learning the things!

--- 2020/1/run1.py
+++ 2020/1/run1.py
@@ -1,6 +1,9 @@
+from functools import reduce
+
 lines = open('input.txt', 'r').readlines()
 for line_a in lines:
     for line_b in lines:
-        if int(line_a) + int(line_b) == 2020:
-            print(int(line_a) * int(line_b))
+        values = [int(line_a), int(line_b)]
+        if sum(values) == 2020:
+            print(reduce((lambda x, y: x * y), values))
             exit(0)
--- 2020/1/run2.py
+++ 2020/1/run2.py
@@ -1,7 +1,10 @@
+from functools import reduce
+
 lines = open('input.txt', 'r').readlines()
 for line_a in lines:
     for line_b in lines:
         for line_c in lines:
-            if int(line_a) + int(line_b) + int(line_c) == 2020:
-                print(int(line_a) * int(line_b) * int(line_c))
+            values = [int(line_a), int(line_b), int(line_c)]
+            if sum(values) == 2020:
+                print(reduce((lambda x, y: x * y), values))
                 exit(0)
